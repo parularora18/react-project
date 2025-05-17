5094b95829616c4a917e0b0b9055151839985b8f
Parul Arora <parularora1809@gmail.com>
2025-05-17 23:17:04 +0530

    React world

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import PostsList from './components/PostsList';
+import MainHeader from './components/MainHeader';
+import {useState} from 'react';
+function App() {
+    const [modalIsVisible, setModalIsVisible ] = useState(true);
+    function showModalHandler() {
+        setModalIsVisible(true);
+    }
+     function hideModalHandler() {
+        setModalIsVisible(false) ;  
+     }
+    return ( 
+        <>
+        <MainHeader onCreatePost={showModalHandler}/>
+        <main>
+           <PostsList  isPosting={modalIsVisible} onStopPosting ={hideModalHandler}/>
+        </main>
+        </>
+    );
+}
+  
+export default App;
---
+++ src/components/PostsList.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import Post from './Post';
+import classes from './PostsList.module.css';
+import NewPost from './NewPost';
+import Modal from './Modal';
+
+function PostsList({isPosting, onStopPosting}) { 
+    const [posts, setPosts] = useState([]);
+
+    function addPostHandler(postData) {
+        setPosts((existingPosts) => [postData, ...existingPosts]);
+    }
+     let modalContent;
+     if (isPosting) {
+      modalContent = (
+        <Modal onClose={onStopPosting}>
+            <NewPost
+             onCancel={onStopPosting} onAddPost= {addPostHandler} />
+         </Modal>
+      );
+    }
+
+        return(
+     <>
+       {/* {modalIsVisible  && ( <Modal onClose={hideModalHandler}>
+            <NewPost
+             onBodyChange= {bodyChangeHandler} 
+             onAuthorChange={authorChangeHandler}/>
+         </Modal>)} */}
+         {modalContent};
+
+         {posts.length > 0 && (
+             <ul className= {classes.posts}> 
+            {posts.map((post) => (<Post key={post.body} author= {post.author} body={post.body} />))}
+        </ul>
+         )}
+      
+        {posts.length === 0 && (
+            <div style={{textAlign: 'center', color:'white'}}>
+                <h2>There are no posts.yet.</h2>
+                <p>Start adding something!</p>
+            </div>
+        )}
+    </> 
+        ) 
+    ;
+}   
+    
+
+export default PostsList; 
